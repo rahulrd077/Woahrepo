d2985bc5c354be8342538cfc600539d3f373927b
Rahul Das <rahulrd077@gmail.com>
2019-01-03 19:42:56 +0530

    Hey There , its my first commit

---
+++ src/main/java/paradighm/TestProj/NewClass.java
@@ -0,0 +1,12 @@
+package paradighm.TestProj;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("RAAH");
+
+	}
+
+}
---
+++ src/test/java/paradighm/TestProj/TestClass.java
@@ -0,0 +1,13 @@
+package paradighm.TestProj;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Man-O-Man");
+		
+		
+	}
+
+}
